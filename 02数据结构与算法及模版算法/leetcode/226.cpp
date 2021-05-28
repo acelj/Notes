617b9455053bd6f89a9f70c0e991678c9e073b53
liujie <1558161609@qq.com>
2021-05-28 23:57:45 +0800

    auto

---
+++ 02数据结构与算法及模版算法/leetcode/226.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return nullptr;
+        swap(root->left, root->right);
+        invertTree(root->left), invertTree(root->right);
+        return root;
+    }
+};
