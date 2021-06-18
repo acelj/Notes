c2639fbae95f0b597036e804a91c072196c7a9d6
liujie <1558161609@qq.com>
2021-06-18 22:42:41 +0800

    auto

--- 02数据结构与算法及模版算法/leetcode/560.cpp
+++ 02数据结构与算法及模版算法/leetcode/560.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
-    int hammingDistance(int x, int y) {
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> s(n + 1);
+        for(int i = 1; i <= n; i++) s[i] = s[i - 1] + nums[i - 1];
+
+        unordered_map<int, int> hash;
+        hash[0] = 1;
         int re = 0;
-        while(x || y)
+        for(int i = 1; i <= n; i++)
         {
-            re += (x & 1 ) ^ (y & 1);
-            x >>= 1, y >>= 1;
+            re += hash[s[i] - k];  // 前缀和的转换， 时间复杂度为O(n)
+            hash[s[i]] ++;
         }
         return re;
     }
